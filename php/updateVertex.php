e4aa036b7cce9bdcc4e72ac317ed92052661404e
tianyishinidie <32159729+tianyishinidie@users.noreply.github.com>
2019-09-04 15:09:17 +0800

    update vertex

--- php/updateVertex.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require 'dblogin.php';
-	$vname=$_POST['vertexname'];
-	$vdesc=$_POST['vertexdescript'];
-	$result=mysqli_query($con,"update vertexinfo set name='{$vname}',descript='{$vdesc}' where name='{$vname}'");
-	
-
-?>
