6bd36fa4356fb831027d7832ef57209f0a924b6b
tianyishinidie <32159729+tianyishinidie@users.noreply.github.com>
2019-09-04 10:55:58 +0800

    Update addVertex.php

--- php/addVertex.php
+++ php/addVertex.php
@@ -4,6 +4,8 @@
 	$x=$_POST['x'];
 	$y=$_POST['y'];
 	$edges=$_POST['edges'];
+	$noname=$_POST['noName'];
+	
 	require 'dblogin.php';
 	//查询点id
 	$vidres=mysqli_query($con,"
@@ -12,12 +14,36 @@
 	$vidrow=mysqli_fetch_array($vidres);
 	if($vid=="")	$vid=0;
 	$vid=$vidrow['vid']+1;
+//是路口
+if($noname=="on")
+{
+	$ishide=1;
+	$vname="cross".$vid;
+}
+else
+{
+	$ishide=0;
+}
 	//插入点信息
+	$sql="
+		insert into vertexinfo
+		values
+		({$vid},'{$vname}','{$vdesc}',{$ishide},{$x},{$y})
+	;";
+	echo $sql;
+	//exit();
 	$insertvres=mysqli_query($con,"
 		insert into vertexinfo
 		values
-		({$vid},'{$vname}','{$vdesc}',0,{$x},{$y})
+		({$vid},'{$vname}','{$vdesc}',{$ishide},{$x},{$y})
 	;");
+	
+	if(!$insertvres)
+	{
+		echo mysqli_error($con);
+		exit();
+	}
+	
 	//插入边信息
 	foreach ($edges as $newedge)
 	{
